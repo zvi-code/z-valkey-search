2cf882201edfba2c0e985f6d3a46ef5590ba7a83
yairgott <yairgott@gmail.com>
2025-02-08 21:58:14 +0000

    fixing clang tidy github workflow

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Get the current user's ID, group ID, and username
 user_id=$(id -u)
-group_id=$(id -g)
 user_name=$(whoami)
+group_id=$(id -g)
+group_name=$(id -gn)
 
 # Define source and destination file paths
 source_file=".devcontainer/devcontainer_base.json"
@@ -19,7 +19,8 @@ fi
 sed -e "s/\"USER_UID\": \"1000\"/\"USER_UID\": \"$user_id\"/g" \
     -e "s/\"USER_GID\": \"1000\"/\"USER_GID\": \"$group_id\"/g" \
     -e "s/\"remoteUser\": \"ubuntu\"/\"remoteUser\": \"$user_name\"/g" \
-    -e "s/\"USERNAME\": \"ubuntu\"/\"USERNAME\": \"$user_name\"/g" \
+    -e "s/\"USER_NAME\": \"ubuntu\"/\"USER_NAME\": \"$user_name\"/g" \
+    -e "s/\"USER_GNAME\": \"ubuntu\"/\"USER_GNAME\": \"$group_name\"/g" \
     -e "s/USER=ubuntu/USER=$user_name/g" \
     "$source_file" > "$destination_file"
 
@@ -27,8 +28,9 @@ sed -e "s/\"USER_UID\": \"1000\"/\"USER_UID\": \"$user_id\"/g" \
 if [[ $? -eq 0 ]]; then
     echo "File successfully created at $destination_file with updated user information:"
     echo "  USER_UID: $user_id"
+    echo "  USER_UNAME: $user_name"
     echo "  USER_GID: $group_id"
-    echo "  remoteUser: $user_name"
+    echo "  USER_GNAME: $user_name"
 else
     echo "Failed to create the file $destination_file."
     exit 2
