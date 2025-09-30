ae6464f2605f11a70c06b27a3a123839ed1eb054
Yana Molodetsky <59420437+yanamolo@users.noreply.github.com>
2025-09-30 18:08:53 +0300

    Fix integration framework for JSON index creation (#399)
    * Fix integration framework for JSON index creation
    
    Signed-off-by: Yana Molodetsky <yamolodu@amazon.com>

--- integration/test_json_operations.py
+++ integration/test_json_operations.py
@@ -27,7 +27,7 @@ def sum_of_remote_searches(nodes: list[Node]) -> int:
 def do_json_backfill_test(test, client, primary, replica):
     assert(primary.info("replication")["role"] == "master")
     assert(replica.info("replication")["role"] == "slave")
-    index = Index("test", [Vector("v", 3, type="FLAT")], type="JSON")
+    index = Index("test", [Vector("v", 3, type="FLAT")], type=KeyDataType.JSON)
     index.load_data(client, 100)
     replica.readonly()
     assert(primary.execute_command("DBSIZE") > 0)
@@ -73,3 +73,36 @@ class TestSearchFTDropindexCMD(ValkeySearchTestCaseDebugMode):
     def test_json_backfill_CMD(self):
         do_json_backfill_test(self, self.client, self.get_primary_connection(), self.get_replica_connection(0))
 
+class TestCreateNonVectorIndexes(ValkeySearchClusterTestCase):
+    """
+    Test create and search for JSON non-vector indexes
+    """
+    def test_non_vector_indexes(self):
+        numeric_indx_name = "numeric"
+        tag_idx_name = "tag"
+        client = self.new_cluster_client()
+        index_numeric = Index(numeric_indx_name, [Numeric("n")], type=KeyDataType.JSON)
+        index_tag = Index(tag_idx_name, [Tag("t")], type=KeyDataType.JSON)
+        indexes = [index_numeric, index_tag]
+        for index in indexes:
+            index.load_data(client, 10)
+            index.create(client)
+            for primary_client in self.get_all_primary_clients():
+                waiters.wait_for_true(lambda: index_on_node(primary_client, index.name))
+                waiters.wait_for_true(lambda: index.backfill_complete(primary_client))
+        
+        # Run numeric query
+        numeric_result = client.execute_command(
+            "FT.SEARCH",
+            numeric_indx_name,
+            f"@n:[-inf +inf]"
+        )
+        assert numeric_result[0] == 10
+        # Run tag query
+        tag_result = client.execute_command(
+            "FT.SEARCH",
+            tag_idx_name,
+            f"@t:{{Tag:*}}"
+        )
+        assert tag_result[0] == 10
+        
